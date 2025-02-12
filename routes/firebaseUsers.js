26bc7d38787b75d506857cac433db4891d220360
anonnymous256 <af083008@gmail.com>
2025-02-11 22:31:09 -0300

    teste 2

--- routes/firebaseUsers.js
+++ routes/firebaseUsers.js
@@ -4,6 +4,7 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("../serviceAccountKey.json");
 
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
